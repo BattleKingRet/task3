076dd11911afffad6ceeb0a9bf6ef7e04d9d5c24
BattleKingRet <54021130+BattleKingRet@users.noreply.github.com>
2020-05-25 19:37:03 +0530

    fvfvet

--- tensorflow.py
+++ tensorflow.py
@@ -23,16 +23,11 @@ batch_size = 32
 #epochs = 200
 data_augmentation = True
 
-# network parameters
 num_classes = 10
 num_dense_blocks = 3
 use_max_pool = False
 
-# DenseNet-BC with dataset augmentation
-# Growth rate   | Depth |  Accuracy (paper)| Accuracy (this)      |
-# 12            | 100   |  95.49%          | 93.74%               |
-# 24            | 250   |  96.38%          | requires big mem GPU |
-# 40            | 190   |  96.54%          | requires big mem GPU |
+
 growth_rate = 12
 depth = 100
 num_bottleneck_layers = (depth - 4) // (2 * num_dense_blocks)
@@ -40,13 +35,10 @@ num_bottleneck_layers = (depth - 4) // (2 * num_dense_blocks)
 num_filters_bef_dense_block = 2 * growth_rate
 compression_factor = 0.5
 
-# load the CIFAR10 data
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-# input image dimensions
 input_shape = x_train.shape[1:]
 
-# mormalize data
 x_train = x_train.astype('float32') / 255
 x_test = x_test.astype('float32') / 255
 print('x_train shape:', x_train.shape)
@@ -54,19 +46,11 @@ print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 print('y_train shape:', y_train.shape)
 
-# convert class vectors to binary class matrices.
 y_train = to_categorical(y_train, num_classes)
 y_test = to_categorical(y_test, num_classes)
 
 def lr_schedule(epoch):
-    """Learning Rate Schedule
-    Learning rate is scheduled to be reduced after 80, 120, 160, 180 epochs.
-    Called automatically every epoch as part of callbacks during training.
-    # Arguments
-        epoch (int): The number of epochs
-    # Returns
-        lr (float32): learning rate
-    """
+
     lr = 1e-3
     if epoch > 180:
         lr *= 0.5e-3
@@ -80,8 +64,6 @@ def lr_schedule(epoch):
     return lr
 
 
-# start model definition
-# densenet CNNs (composite function) are made of BN-ReLU-Conv2D
 inputs = Input(shape=input_shape)
 x = BatchNormalization()(inputs)
 x = Activation('relu')(x)
@@ -91,9 +73,7 @@ x = Conv2D(num_filters_bef_dense_block,
            kernel_initializer='he_normal')(x)
 x = concatenate([inputs, x])
 
-# stack of dense blocks bridged by transition layers
 for i in range(num_dense_blocks):
-    # a dense block is a stack of bottleneck layers
     for j in range(num_bottleneck_layers):
         y = BatchNormalization()(x)
         y = Activation('relu')(y)
@@ -113,11 +93,9 @@ for i in range(num_dense_blocks):
             y = Dropout(0.2)(y)
         x = concatenate([x, y])
 
-    # no transition layer after the last dense block
     if i == num_dense_blocks - 1:
         continue
 
-    # transition layer compresses num of feature maps and reduces the size by 2
     num_filters_bef_dense_block += num_bottleneck_layers * growth_rate
     num_filters_bef_dense_block = int(num_filters_bef_dense_block * compression_factor)
     y = BatchNormalization()(x)
@@ -130,16 +108,12 @@ for i in range(num_dense_blocks):
     x = AveragePooling2D()(y)
 
 
-# add classifier on top
-# after average pooling, size of feature map is 1 x 1
 x = AveragePooling2D(pool_size=8)(x)
 y = Flatten()(x)
 outputs = Dense(num_classes,
                 kernel_initializer='he_normal',
                 activation='softmax')(y)
 
-# instantiate and compile model
-# orig paper uses SGD but RMSprop works better for DenseNet
 model = Model(inputs=inputs, outputs=outputs)
 model.compile(loss='categorical_crossentropy',
               optimizer=RMSprop(1e-3),
@@ -147,14 +121,12 @@ model.compile(loss='categorical_crossentropy',
 model.summary()
 plot_model(model, to_file="cifar10-densenet.png", show_shapes=True)
 
-# prepare model model saving directory
 save_dir = os.path.join(os.getcwd(), 'saved_models')
 model_name = 'cifar10_densenet_model.{epoch:02d}.h5'
 if not os.path.isdir(save_dir):
     os.makedirs(save_dir)
 filepath = os.path.join(save_dir, model_name)
 
-# prepare callbacks for model saving and for learning rate reducer
 checkpoint = ModelCheckpoint(filepath=filepath,
                              monitor='val_acc',
                              verbose=1,
@@ -169,7 +141,6 @@ lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1),
 
 callbacks = [checkpoint, lr_reducer, lr_scheduler]
 
-# run training, with or without data augmentation
 if not data_augmentation:
     print('Not using data augmentation.')
     model.fit(x_train, y_train,
@@ -180,25 +151,22 @@ if not data_augmentation:
               callbacks=callbacks)
 else:
     print('Using real-time data augmentation.')
-    # preprocessing  and realtime data augmentation
     datagen = ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the dataset
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of dataset
-        samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        rotation_range=0,  # randomly rotate images in the range (deg 0 to 180)
-        width_shift_range=0.1,  # randomly shift images horizontally
-        height_shift_range=0.1,  # randomly shift images vertically
-        horizontal_flip=True,  # randomly flip images
-        vertical_flip=False)  # randomly flip images
+        featurewise_center=False,  
+        samplewise_center=False,  
+        featurewise_std_normalization=False,  
+        samplewise_std_normalization=False,  
+        zca_whitening=False, 
+        rotation_range=0,  
+        width_shift_range=0.1, 
+        height_shift_range=0.1,  
+        horizontal_flip=True, 
+        vertical_flip=False)  
 
-    # compute quantities required for featurewise normalization
-    # (std, mean, and principal components if ZCA whitening is applied)
+    
     datagen.fit(x_train)
 
     steps_per_epoch = math.ceil(len(x_train) / batch_size)
-    # fit the model on the batches generated by datagen.flow().
     model.fit(x=datagen.flow(x_train, y_train, batch_size=batch_size),
               verbose=1,
               epochs=epochs,
@@ -207,14 +175,7 @@ else:
               callbacks=callbacks)
 
 
-    # fit the model on the batches generated by datagen.flow()
-    #model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),
-    ##                    steps_per_epoch=x_train.shape[0] // batch_size,
-    #                    validation_data=(x_test, y_test),
-    #                    epochs=epochs, verbose=1,
-    #                    callbacks=callbacks)
-
-# score trained model
+   
 scores = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', scores[0])
 print('Test accuracy:', scores[1])
